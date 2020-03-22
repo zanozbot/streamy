c8ebe14b0a244e41a75494503defbfd016de7976
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-22 21:32:03 +0100

    Added custom overlay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -8,6 +7,7 @@ import { environment } from '../environments/environment';
 import { HeaderComponent } from './content/header/header.component';
 import { PlayerComponent } from './content/player/player.component';
 import { FloatingButtonComponent } from './content/floating-button/floating-button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,8 @@ import { FloatingButtonComponent } from './content/floating-button/floating-butt
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
