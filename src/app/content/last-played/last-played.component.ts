4accd88ef6a20589cfbb91656def08d27654478f
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-25 18:10:27 +0100

    Added animation to song list

--- src/app/content/last-played/last-played.component.ts
+++ src/app/content/last-played/last-played.component.ts
@@ -2,11 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerService } from 'src/app/services/player.service';
 import { Observable } from 'rxjs';
 import { Song } from 'src/models/song.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-last-played',
   templateUrl: './last-played.component.html',
-  styleUrls: ['./last-played.component.scss']
+  styleUrls: ['./last-played.component.scss'],
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('fade', [
+      // the "in" style determines the "resting" state of the element when it is visible.
+      state('in', style({ opacity: 1 })),
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms ease')
+      ]),
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate('250ms ease', style({ opacity: 0 })))
+    ])
+  ]
 })
 export class LastPlayedComponent implements OnInit {
 
