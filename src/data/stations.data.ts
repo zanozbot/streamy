256bba20d8bed0be488e3f43d901ba422312f2a2
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-26 18:17:04 +0100

    Removed data

--- src/data/stations.data.ts
+++ src/data/stations.data.ts
@@ -5,19 +5,5 @@ import { Station } from 'src/models/station.model';
  * Replace this with your links
  */
 export const stations: Station[] = [
-  {
-    name: 'Classic Rock Florida HD',
-    stream: 'http://198.58.98.83:8258/stream',
-    lastSongsPath: 'http://198.58.98.83:8258/played?type=json'
-  },
-  {
-    name: 'Dance UK Radio',
-    stream: 'http://uk2.internet-radio.com:8024/stream',
-    lastSongsPath: 'http://uk2.internet-radio.com:8024/played?type=json'
-  },
-  {
-    name: 'Pigpen Radio',
-    stream: 'http://178.79.158.160:8213/stream',
-    lastSongsPath: 'http://178.79.158.160:8213/played?type=json'
-  }
+
 ];
