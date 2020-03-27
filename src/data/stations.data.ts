7bf58b06b8c1e56d6c52383fec52fcba5cb346e3
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-27 10:25:52 +0100

    Prepared for Aktual

--- src/data/stations.data.ts
+++ src/data/stations.data.ts
@@ -5,5 +5,27 @@ import { Station } from 'src/models/station.model';
  * Replace this with your links
  */
 export const stations: Station[] = [
-
+  {
+    name: 'Osrednje slovenska',
+    stream: 'http://stream.radioaktual.si/Aktual',
+    lastSongsPath: 'http://api2.kme.si/v2/radio-live/aktual'
+  },
+  {
+    name: 'Obala',
+    stream: 'http://stream.radioaktual.si/AktualO',
+    lastSongsPath: 'http://api2.kme.si/v2/radio-live/aktual-osrednja'
+  },
+  {
+    name: 'Studio D',
+    stream: 'http://stream.radioaktual.si/AktualD',
+    lastSongsPath: 'http://api2.kme.si/v2/radio-live/aktual-osrednja'
+  },
+  {
+    name: 'Dalmacija',
+    stream: 'http://stream.radioaktual.si/AktualDalmacija'
+  },
+  {
+    name: 'Latino',
+    stream: 'http://stream.radioaktual.si/AktualLatino'
+  }
 ];
