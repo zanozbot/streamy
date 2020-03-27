3916f3994d997430dcb5fec6422b4a28eb608678
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-27 13:22:34 +0100

    Removed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
